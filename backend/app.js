4ffd8a700dbcc576817457a659e98df59d6c9b33
yoppp11 <yopinur040903@gmail.com>
2025-03-26 15:02:00 +0700

    setup deploy

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+// require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
  
 const app = express()
-const port = 3000
+
 
 
 app.use(express.urlencoded({extended: false}))
@@ -14,6 +17,6 @@ app.use(cors())
 app.use('/', require('./routers/index'))
 
 
-app.listen(port, ()=> {
-    console.log(`lari bg ada meteor ${port}`);
-})
+module.exports = {
+    app
+}
